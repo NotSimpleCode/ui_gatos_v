ab624403c3742b52f11132cbedf35f7ab65a2ce8
NotSimpleCode <onejobsdiego@gmail.com>
2025-06-17 09:46:55 -0500

    button back table to selector

--- src/app/cats/breed-table/breeds-table.component.ts
+++ src/app/cats/breed-table/breeds-table.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CatsService } from '../../core/services/cats.service';
-import { CommonModule } from '@angular/common';
+import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FilterBreedsPipe } from '../../pipes/text-filter.pipe';
+
 
 @Component({
   standalone: true,
@@ -16,12 +17,16 @@ export class BreedsTableComponent implements OnInit {
   filterText = '';
   searchText = '';
 
-  constructor(private readonly catsService: CatsService) {}
+  constructor(private readonly catsService: CatsService, private readonly location: Location) {}
 
   ngOnInit() {
     this.catsService.getBreeds().subscribe(data => this.breeds = data);
   }
   onSearch() {
     this.searchText = this.filterText;
+  }
+
+  goBack() {
+    this.location.back();
   }
 }
